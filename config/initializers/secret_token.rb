8b9afdc4da0de6f9d621c14c234c219351f5f109
pjambet <pierre.jambet@gmail.com>
2013-04-07 15:01:04 -0400

    Try to use an ENV var for secret_key_base if it exists

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-BeechWeb::Application.config.secret_key_base = '1a1c2f2e88abe3f5276437b9b0783dfcef170f355ad410afa2edc2c614e93c347aafe91e81f19e46e83e8a4880bfa60af41affc88dd109b4da04bb80129ae848'
+secret_key_base = ENV['SECRET_KEY_BASE'] || '1a1c2f2e88abe3f5276437b9b0783dfcef170f355ad410afa2edc2c614e93c347aafe91e81f19e46e83e8a4880bfa60af41affc88dd109b4da04bb80129ae848'
+BeechWeb::Application.config.secret_key_base = secret_key_base
